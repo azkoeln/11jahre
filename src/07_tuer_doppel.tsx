3fb17ec52e2ef21a878e7bdc3f8afeb6a93e7083
ilsend <49043963+ilsend@users.noreply.github.com>
2021-04-12 15:03:41 +0200

    added objects

--- src/07_tuer_doppel.tsx
+++ src/07_tuer_doppel.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.5.0" name="07_tuer_doppel" tilewidth="32" tileheight="32" tilecount="4" columns="2">
- <image source="../../../world-map-starterkit-master/world-map-starterkit-master/tiles_png/community/cert/imgs/doors/07_tuer_doppel.png" trans="ff00ff" width="64" height="64"/>
+ <image source="src/07_tuer_doppel.png" trans="ff00ff" width="64" height="64"/>
 </tileset>
--- src/rc3-mullana-objects.tsx
+++ src/rc3-mullana-objects.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.5.0" name="rc3-mullana-objects" tilewidth="32" tileheight="32" tilecount="128" columns="16">
- <image source="../../../world-map-starterkit-master/world-map-starterkit-master/tiles_png/deco/rc3-mullana-objects.png" trans="ff00ff" width="512" height="256"/>
+ <image source="src/rc3-mullana-objects.png" trans="ff00ff" width="512" height="256"/>
 </tileset>
